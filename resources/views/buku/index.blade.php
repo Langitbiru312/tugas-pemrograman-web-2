80d056170b6cac16662dabd23c5578aacb76eb4d
Yelma Rapiani <yelmarapiani@gmail.com>
2026-04-27 15:10:09 +0800

    feat:penyiapan blade component untuk layout utama

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,9 @@
+<x-app>
+
+
+    <x-slot:title> {{ $title }}</x-slot>
+
+
+
+    <h1 class="fw-bold">Data Buku</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/buku', function () {
+    return view('buku.index', ['title' => 'Buku']);
+});
+Route::get('/buku/create', function () {
+    return view('buku.create', ['title' => 'Create Buku']);
+});
